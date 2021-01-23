9e980ea56cc79b939507a8ba57c117320c64d242
jadeduo <2463870651@qq.com>
2021-01-23 19:09:47 +0800

    Update 1005.py

--- 1005.py
+++ 1005.py
@@ -1,7 +1,20 @@
 #3*n+1猜想
-n=int(input())
-num=[]
-num=input().split()
+# n=int(input())
+# num=[]
+# num=input().split()
 
 #列表num保存n个数
 
+
+            
+                
+            
+        
+        
+        
+            
+            
+        
+        
+        
+        
